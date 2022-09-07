38f0010c189263489c8690e823db2197849a1e06
Lucien Shaw <myxlc55@outlook.com>
2022-09-08 01:14:15 +0800

    feat: 添加了自动登录webvpn获取cookie的功能
    通过了CPU的统一认证登录

--- src/webvpn.py
+++ src/webvpn.py
@@ -1,4 +1,5 @@
 import getpass
+import requests
 
 def is_exec():
     import sys
@@ -17,6 +18,64 @@ def get_dir(myfile=__file__):
     return mydir
 
 MYDIR = get_dir()
+HEADERS = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.27',
+        'sec-ch-ua': '"Microsoft Edge";v="105", " Not;A Brand";v="99", "Chromium";v="105"',
+        'sec-ch-ua-arch': '"x86"',
+        'sec-ch-ua-bitness': '"64"',
+        'sec-ch-ua-full-version': '"105.0.1343.27"',
+        'sec-ch-ua-full-version-list': '"Microsoft Edge";v="105.0.1343.27", " Not;A Brand";v="99.0.0.0", "Chromium";v="105.0.5195.96"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-model': '""',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-ch-ua-platform-version': '"14.0.0"',
+    }
+
+def cpu_webvpn_login(credentials):
+    s = requests.Session()
+    headers = HEADERS.copy()
+
+    response = s.get('https://webvpn.cpu.edu.cn/portal', headers=headers)
+    mycookie = response.history[0].cookies.get_dict()['wengine_vpn_ticketwebvpn_cpu_edu_cn']
+
+    headers['Origin'] = 'https://webvpn.cpu.edu.cn'
+    headers['Referer'] = 'https://webvpn.cpu.edu.cn/https/77726476706e69737468656265737421f9f30f9f372526557a1dc7af96/sso/login?service=https%3A%2F%2Fwebvpn.cpu.edu.cn%2Flogin%3Fcas_login%3Dtrue'
+    params = {'service': 'https://webvpn.cpu.edu.cn/login?cas_login=true',}
+    data = {
+        'lt': '${loginTicket}',
+        'useVCode': '',
+        'isUseVCode': 'true',
+        'sessionVcode': '',
+        'errorCount': '',
+        'execution': 'e1s1',
+        'service': 'https://webvpn.cpu.edu.cn/login?cas_login=true',
+        '_eventId': 'submit',
+        'geolocation': '',
+        'username': credentials[0],
+        'password': credentials[1],
+        'rememberpwd': 'on',
+    }
+    
+    response = s.post('https://webvpn.cpu.edu.cn/https/77726476706e69737468656265737421f9f30f9f372526557a1dc7af96/sso/login', params=params,headers=headers, data=data)
+    return mycookie
+
+def web_go(url,cookie):
+    import requests
+    cookies = {
+        'wengine_vpn_ticketwebvpn_cpu_edu_cn': cookie,
+    }
+    headers = HEADERS.copy()
+    response = requests.get(url, cookies=cookies, headers=headers)
+    return response
 
 def get_credentials():
     cred = []
@@ -39,4 +98,8 @@ def get_credentials():
 
 if __name__ == '__main__':
     # Test only
-    print(get_credentials())
+    url = 'https://webvpn.cpu.edu.cn/https/77726476706e69737468656265737421f3f90f9e2e3e6f1e7d0784/'
+    cred = get_credentials()
+    mycookie = cpu_webvpn_login(cred)
+    resp = web_go(url,mycookie)
+    print(resp.text)
