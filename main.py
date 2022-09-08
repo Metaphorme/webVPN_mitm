b37cf79ee722beb7e7a2b5f657b0edf18688fef1
Lucien Shaw <myxlc55@outlook.com>
2022-09-08 15:24:12 +0800

    test: 建立主程序入口

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from metaphor_tunnel import url_conversion as urlc
+from metaphor_tunnel import webvpn as wvpn
 
 def is_exec():
     if getattr(sys, 'frozen', False):
@@ -13,3 +15,14 @@ def get_dir(myfile=__file__):
     else:
         mydir=os.path.dirname(os.path.abspath(myfile))
     return mydir
+MYDIR = get_dir(__file__)
+
+def main():
+    url = urlc.get_url(urlc.get_url_info('https://cn.bing.com'))
+    print(url)
+    cred = wvpn.get_credentials()
+    mycookie = wvpn.cpu_webvpn_login(cred)
+    print(mycookie)
+
+if __name__ == '__main__':
+    main()
