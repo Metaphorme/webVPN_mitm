790d2fed5e3afeb6033efe35f9b6174b59120817
Metaphorme <102651183+metaphorme@users.noreply.github.com>
2022-09-18 20:58:44 +0800

    feat:工具菜单

--- main.py
+++ main.py
@@ -1,30 +1,157 @@
+# -*- coding: utf-8 -*-
 import os
-import sys
-from metaphor_tunnel import url_conversion as urlc
-from metaphor_tunnel import cpu_webvpn as cvpn
 
-def is_exec():
-    if getattr(sys, 'frozen', False):
+
+def clear():
+    # For Windows Users
+    if os.name == "nt":
+        os.system('clr')
+    # For other Users
+    else:
+        os.system('clear')
+
+
+def find_cert(file_path='credentials.txt'):
+    if os.path.exists(file_path):
         return True
     else:
         return False
 
-def get_dir(myfile=__file__):
-    if is_exec():
-        mydir=os.path.dirname(os.path.abspath(sys.executable))
-    else:
-        mydir=os.path.dirname(os.path.abspath(myfile))
-    return mydir
 
-MYDIR = get_dir(__file__)
-INST_HOSTNAME = 'webvpn.cpu.edu.cn'
+def write_cert():
+    from webVPN_mitm.cpu_webvpn import write_credentials
+    import getpass
+
+    username = input("请输入学号：")
+    password = getpass.getpass("请输入密码：")
+
+    write_credentials(username=username, password=password, file_path='credentials.txt')
+    print("已创建凭证文件 credentials.txt")
+
+
+def verify_cert():
+    from webVPN_mitm.cpu_webvpn import read_credentials
+    from webVPN_mitm.cpu_webvpn import login
+
+    username, password = read_credentials(file_path='credentials.txt')
+    print("读取成功，学号：{} ，密码：{}".format(username, password))
+    print("正在尝试登录...")
+    my_cookie = login(username=username, password=password)
+    print("获得 Cookie：{wengine_vpn_ticketwebvpn_cpu_edu_cn: " + my_cookie + "}")
+
+
+def url_encode():
+    from webVPN_mitm.url_conversion import WebvpnUrl
+    d = WebvpnUrl(inst_hostname="webvpn.cpu.edu.cn")
+
+    url = input("请输入待转换的普通url：")
+    print("URL_Encoded: " + d.url_encode(url))
+
+
+def url_decode():
+    from webVPN_mitm.url_conversion import WebvpnUrl
+    d = WebvpnUrl()
+
+    url = input("请输入webVPN 加密后的 URL：")
+    print("URL_Decoded: " + d.url_decode(url))
+
+
+def get_cookie():
+    if find_cert():
+        verify_cert()
+    else:
+        import getpass
+        from webVPN_mitm.cpu_webvpn import login
+
+        username = input("请输入学号：")
+        password = getpass.getpass("请输入密码：")
+        print("正在尝试登录...")
+        my_cookie = login(username=username, password=password)
+        print("获得 Cookie：{wengine_vpn_ticketwebvpn_cpu_edu_cn: " + my_cookie + "}")
+
+
+def start_http(port="8080"):
+    try:
+        input_port = int(input("请输入 HTTP/HTTPS 监听端口：（默认：8080）"))
+    except ValueError:
+        input_port = 8080
+    if input_port in range(0, 65536):
+        port = str(input_port)
+    else:
+        print("端口不合法，将使用8080")
+    os.system("mitmdump -s webVPN_mitm/webvpn-mitm.py --listen-port " + port)
+
+
+"""
+def start_sock5(port="1080"):
+    try:
+        input_port = int(input("请输入 sock5 监听端口：（默认：1080）"))
+    except ValueError:
+        input_port = 1080
+    if input_port in range(0, 65536):
+        port = str(input_port)
+    else:
+        print("端口不合法，将使用1080")
+    os.system("mitmdump -s webVPN_mitm/webvpn-mitm.py --mode socks5 --listen-port " + port)
+"""
+
 
 def main():
-    url = urlc.get_url(urlc.get_url_info('ws://121.40.165.18:8800',INST_HOSTNAME))
-    print(url)
-    # cred = cvpn.get_credentials()
-    # mycookie = cvpn.login(cred)
-    # print(mycookie)
+    clear()
+    print()
+    print()
+    print("                     _     _   _______ _   _                  _ _     ")
+    print("                    | |   | | | | ___ \ \ | |                (_) |     ")
+    print("       __      _____| |__ | | | | |_/ /  \| |______ _ __ ___  _| |_ _ __ ___     ")
+    print("       \ \ /\ / / _ \ '_ \| | | |  __/| . ` |______| '_ ` _ \| | __| '_ ` _ \     ")
+    print("        \ V  V /  __/ |_) \ \_/ / |   | |\  |      | | | | | | | |_| | | | | |     ")
+    print("         \_/\_/ \___|_.__/ \___/\_|   \_| \_/      |_| |_| |_|_|\__|_| |_| |_|     ")
+    print()
+    print()
+    print("{:^83}".format("webVPN_mitm, A Good Man In the Middle of You and webVPN"))
+    print("{:^83}".format("Author: Metaphorme, github.com/Metaphorme, in CPU"))
+    print("{:^83}".format("            Lucien Shaw, github.com/lucienshawls, in CPU"))
+    print("{:^83}".format("Designed for webVPN in China Pharmaceutical University"))
+    print("{:^83}".format("Licensed under Mozilla Public License 2.0"))
+    print("{:^83}".format("Repo: https://github.com/Metaphorme/webVPN-mitm"))
+    print()
+    print()
+    print("{:<83}".format("工具选项："))
+    print("{:<83}".format("- 1. 创建凭证文件"))
+    print("{:<83}".format("- 2. 验证凭证文件有效性"))
+    print("{:<83}".format("- 3. 普通 URL -> webVPN 加密 URL"))
+    print("{:<83}".format("- 4. webVPN 加密 URL -> 普通 URL"))
+    print("{:<83}".format("- 5. 获取有效 Cookie"))
+    print("{:<83}".format("- 6. 开启 HTTP/HTTPS 代理服务器"))
+    print()
+
+    try:
+        choose = input("请输入所需工具前的序号（e.g, 1）：")
+    except KeyboardInterrupt:
+        exit()
+
+    if choose == "1":
+        write_cert()
+        main()
+    elif choose == "2":
+        verify_cert()
+        main()
+    elif choose == "3":
+        url_encode()
+        main()
+    elif choose == "4":
+        url_decode()
+        main()
+    elif choose == "5":
+        get_cookie()
+        main()
+    elif choose == "6":
+        start_http()
+        main()
+    else:
+        print("输入不合法，将退出...")
+        exit()
+
 
 if __name__ == '__main__':
     main()
